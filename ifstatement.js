0f28a1d7dafc189a1a1cbbce00d43f159ae691ff
Tenzin <tenthinten@gmail.com>
2021-12-26 23:14:20 +0530

    if statement practice

---
+++ ifstatement.js
@@ -0,0 +1,19 @@
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "yes it is true";
+    }
+    return "no it s false";
+}
+console.log(ourTrueOrFalse(false));
+
+
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "yes";
+    }
+    return"no";
+}
+
+console.log(trueOrFalse(true));
+
+
