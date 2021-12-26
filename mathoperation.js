49cb41aaae412a15a7d6ed1847b071dff748085a
Tenzin <tenthinten@gmail.com>
2021-12-26 20:47:06 +0530

    math operation +,-,/,*,%

---
+++ mathoperation.js
@@ -0,0 +1,91 @@
+var sum = 10 + 10;
+console.log(sum);
+
+
+var difference = 45 - 33;
+console.log(difference);
+
+
+var product = 8 * 10;
+console.log(product);
+
+
+var quotient = 66 / 33;
+console.log(quotient);
+
+// Increment
+var myVar = 87;
+// Only change code below this line
+myVar = myVar + 1;
+myVar++;
+console.log(myVar);
+
+// Decrement
+var myVar = 11;
+myVar = myVar - 1;
+myVar--;
+console.log(myVar);
+
+
+// Decimal(floats)
+var ourDecimal = 5.7;
+// var myDecimal = 0.009;
+console.log(ourDecimal);
+
+// Remainder
+var remainder;
+remainder = 11 % 3;
+console.log(remainder)
+
+// Compound assignment with augmented Addition 
+var a = 3;
+var b = 17;
+var c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Compound assignment with augmented  Subtraction
+var a = 11;
+var b = 9;
+var c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Compound assignment with augmented Multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// Compound assignment with augmented Devision
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a);
+console.log(b);
+console.log(c);
