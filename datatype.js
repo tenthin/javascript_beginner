9831280fd6baa010d895c5334f0adf415d3fc794
Tenzin <tenthinten@gmail.com>
2021-12-26 20:27:36 +0530

    assignment and declaration of var

--- datatype.js
+++ datatype.js
@@ -1,21 +1,47 @@
-/*Data types:
-undefined,null boolean, string , symbol, number and object
-*/
+// /*Data types:
+// undefined,null boolean, string , symbol, number and object
+// */
 var myName = "Beau" //can be use through the whole program
 
-myName = 8
+ myName = 8
 
 let ourName = "freeCodeCamp" //can only be used within the scope of where you declare that
 
-const pi = 3.14 // is the variable that can never change
+ const pi = 3.14 // is the variable that can never change
 
 
 
 var a;  //DECLARING a variable
 var b = 2; //DECLARING and ASSIGNING a variable in one line
-
+console.log(a)
 a = 7;
 
 b = a;
 
 console.log(a)
+
+var a = 5;
+var b = 6;
+var c = "I am a"
+
+a = a +1;
+b = b +1;
+c = c + "string";
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+// Declaration
+var studylyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignment
+studylyCapVar = 10;
+properCamelCase = "a string";
+titleCaseOver = 9000;
+
+console.log(studylyCapVar);
+console.log(properCamelCase);
+console.log(titleCaseOver);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,21 @@
+/*Data types:
+// undefined,null boolean, string , symbol, number and object
+// */
+// var myName = "Beau" //can be use through the whole program
+
+// myName = 8
+
+// let ourName = "freeCodeCamp" //can only be used within the scope of where you declare that
+
+// const pi = 3.14 // is the variable that can never change
+
+
+
+// var a;  //DECLARING a variable
+// var b = 2; //DECLARING and ASSIGNING a variable in one line
+// console.log(a)
+// a = 7;
+
+// b = a;
+
+// console.log(a)
