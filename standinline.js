01a4f8614bf81a1880d2d443c1d7f734b3bfdd27
Tenzin <tenthinten@gmail.com>
2021-12-26 23:08:13 +0530

    stand in line que function

--- standinline.js
+++ standinline.js
@@ -5,4 +5,6 @@ function nextInLine(arr,item) {
     arr.push(item);
     return arr.shift();
 }
+console.log("Before " + JSON.stringify(testArr));
 console.log(nextInLine(testArr,6));
+console.log("After " + JSON.stringify(testArr));
