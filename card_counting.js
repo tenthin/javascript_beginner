765e9600dd0f80befe7187d7c995a6010979bfbf
Tenzin <tenthinten@gmail.com>
2021-12-27 20:44:50 +0530

    card game

---
+++ card_counting.js
@@ -0,0 +1,31 @@
+var count = 0;
+
+function cardgame(card) {
+
+    switch (card) {
+        case 2:
+        case 3:
+        case 4: 
+        case 5:
+        case 6:
+            count++
+            break;
+
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--
+            break;
+    }
+    var holdbet = 'Hold';
+
+    if (count > 0) {
+
+        holdbet = 'Bet';
+    }
+    return count + " " + holdbet;
+
+}
+console.log(cardgame(10))
