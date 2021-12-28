6b79a06cfef8a6fbf6a48235b99372e86c23e6da
Tenzin <tenthinten@gmail.com>
2021-12-28 18:30:23 +0530

    nested for loops

--- loops.js
+++ loops.js
@@ -16,8 +16,54 @@ for (var i = 0; i < 5; i++) {
 console.log(ourArray)
 
 
-
 for (var i = 1; i < 6; i++) {
     myArray.push(i);
 }
 console.log(myArray);
+
+
+// Iterate Odd Numbers with FOr loops
+var ourArray = [];
+ for (var i = 0; i < 10; i += 2) {
+     ourArray.push(i);
+ }
+ console.log(ourArray)
+
+ var myArray = [];
+
+ for (var i = 1; i < 10; i += 2) {
+     myArray.push(i);
+ }
+ console.log(myArray)
+
+//  Count Backwards with Forloops
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+    ourArray.push(i);
+}
+console.log(ourArray);
+
+myArray = [];
+for (var i = 9; i > 0; i -= 2) {
+    myArray.push(i);
+}
+console.log(myArray)
+
+
+// Iterate through an Array with a For loops
+var ourArr = [9,10,11,12];
+var ourTotal = 0;
+
+for(var i = 0; i < ourArr.length; i++) {
+    ourTotal += ourArr[i];
+}
+console.log(ourTotal)
+
+
+var myArr = [2,3,4,5,6];
+var myTotal = 0;
+for (var i = 0; i < myArr.length; i++) {
+    myTotal += myArr[i];
+}
+console.log(myTotal);
