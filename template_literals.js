2b3d75ee0f860d7ddd10b393804b375fce594440
Tenzin <tenthinten@gmail.com>
2021-12-29 19:42:06 +0530

    template literals example

--- template_literals.js
+++ template_literals.js
@@ -11,3 +11,20 @@ I am ${person.age} years old.`;
 
 console.log(greeting);
 
+
+// We want to create a list based on the array that passed
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+    const resultDisplayArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        resultDisplayArray.push(`<li class="text-warning">${arr[i]}</li>`)
+    }
+    return resultDisplayArray;
+}
+
+const resultDisplayArray = makeList(result.failure)
+console.log(resultDisplayArray)
